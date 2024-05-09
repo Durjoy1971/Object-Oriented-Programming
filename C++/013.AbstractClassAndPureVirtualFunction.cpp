1ece1a8464ae79525b8ebf88e7b6066a7a57d059
Durjoy1971 <durjoybarua1971@gmail.com>
2024-05-09 22:22:57 +0600

    step 13 done

---
+++ C++/013.AbstractClassAndPureVirtualFunction.cpp
@@ -0,0 +1,69 @@
+/*
+
+> C++ Pure Virtual Functions
+
+Pure virtual functions are used
+>> if a function doesn't have any use in the base class
+>> but the function must be implemented by all its derived classes
+
+> Abstract Class
+>> A class that contains a pure virtual function is known as an abstract class.
+
+*/
+
+// C++ program to calculate the area of a square and a circle
+
+#include <iostream>
+using namespace std;
+
+// Abstract class
+class Shape
+{
+protected:
+    float dimension;
+
+public:
+    void getDimension()
+    {
+        cin >> dimension;
+    }
+
+    // pure virtual Function
+    virtual float calculateArea() = 0;
+};
+
+// Derived class
+class Square : public Shape
+{
+public:
+    float calculateArea()
+    {
+        return dimension * dimension;
+    }
+};
+
+// Derived class
+class Circle : public Shape
+{
+public:
+    float calculateArea()
+    {
+        return 3.14 * dimension * dimension;
+    }
+};
+
+int main()
+{
+    Square square;
+    Circle circle;
+
+    cout << "Enter the length of the square: ";
+    square.getDimension();
+    cout << "Area of square: " << square.calculateArea() << endl;
+
+    cout << "\nEnter radius of the circle: ";
+    circle.getDimension();
+    cout << "Area of circle: " << circle.calculateArea() << endl;
+
+    return 0;
+}
